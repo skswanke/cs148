95b87c8641af0d1136e1adc916676a87b6cedd1f
skswanke <skswanke@gmail.com>
2015-09-22 13:22:03 -0400

    q04

--- assignment2.0/q04.php
+++ assignment2.0/q04.php
@@ -3,6 +3,7 @@
     $columns = 5;
     $query = 'SELECT * FROM tblCourses WHERE fldCourseNumber=148 AND fldDepartment="CS"';
     //public function testquery($query, $values = "", $wheres = 0, $conditions = 0, $quotes = 0, $symbols = 0, $spacesAllowed = false, $semiColonAllowed = false)
+    $info2 = $thisDatabaseReader->testquery($query, "", 1, 1, 2, 0, false, false);
     $info2 = $thisDatabaseReader->select($query, "", 1, 1, 2, 0, false, false);
     print "<h1>Total Records: " .count($info2) ."</h1>";
     print "<h2>Query: " .$query ."</h2>";
