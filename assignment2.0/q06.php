08ad64d6c5dc20a98eaffaea9d00e8054eaa67a7
skswanke <skswanke@gmail.com>
2015-09-22 13:24:13 -0400

    q06

--- assignment2.0/q06.php
+++ assignment2.0/q06.php
@@ -3,6 +3,7 @@
     $columns = 1;
     $query = 'SELECT fldCourseName FROM tblCourses WHERE fldCourseName LIKE "%data%" AND fldDepartment != "CS"';
     //public function testquery($query, $values = "", $wheres = 0, $conditions = 0, $quotes = 0, $symbols = 0, $spacesAllowed = false, $semiColonAllowed = false)
+    $info2 = $thisDatabaseReader->testquery($query, "", 1, 2, 4, 2, false, false);
     $info2 = $thisDatabaseReader->select($query, "", 1, 2, 4, 2, false, false);
     print "<h1>Total Records: " .count($info2) ."</h1>";
     print "<h2>Query: " .$query ."</h2>";
